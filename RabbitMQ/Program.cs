c2ed0e3b5f420f1233c9997eabf9178466ce110f
Paweł Pałasz <ppalasz@poczta.onet.pl>
2019-03-30 11:27:40 +0100

    commit 2

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
 
-namespace RabbitMQ
+namespace RabbitSender
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Rabbit Sender");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            // otwarcie połączenia
+            using (var connection = factory.CreateConnection())
+            {
+                // utworzenie kanału komunikacji
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "msgKey",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    //console.WriteLine("Wprowadz wiadomość do wysłania: ");
+                    for (int i = 0; i < 200; i++)
+                    {
+                        string msg = "message " + i; //Console.ReadLine();
+                        var msgBody = Encoding.UTF8.GetBytes(msg);
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "msgKey",
+                            basicProperties: null,
+                            body: msgBody);
+                        Console.WriteLine($" [x] wysłano '{msg}'");
+
+                        //Console.WriteLine("ConsumerCount=" + channel.ConsumerCount("msgKey"));
+
+
+                        Thread.Sleep(2000);
+                    }
+                }
+
+                Console.WriteLine("...press [Enter] to end");
+                Console.ReadLine();
+            }
         }
     }
 }
---
+++ RabbitReceiver/Program.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace RabbitReceiver
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Rabbit Receiver");
+
+            //Console.WriteLine("...press [Enter] to read");
+            //Console.ReadLine();
+
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost" };
+            // otwarcie połączenia
+            IConnection connection = factory.CreateConnection();
+
+            // utworzenie kanału komunikacji
+            IModel channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "msgKey",
+                durable: false,
+                exclusive: false,
+                autoDelete: false,
+                arguments: null);
+
+            EventingBasicConsumer consumer = CreateConsumer(channel);
+
+           // Thread.Sleep(10000);
+
+            //Console.WriteLine("...press [Enter] to end");
+            //Console.ReadLine();
+        }
+
+        public static EventingBasicConsumer CreateConsumer(IModel channel)
+        {
+            EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+
+            consumer.Shutdown += (d, e) =>
+            {
+                Console.WriteLine($"ShutdownReason '{consumer.ShutdownReason}'");
+                //consumer = CreateConsumer(channel);
+            };
+            Thread.Sleep(1000);
+
+            // for (int i = 0; i < 10; i++)
+            {
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($" [x] otrzymano '{message}'");
+                    // Console.WriteLine("ConsumerCount=" + channel.MessageCount("msgKey"));
+                   // Thread.Sleep(100);
+                };
+
+                channel.BasicConsume(queue: "msgKey",
+                    autoAck: true,
+                    consumer: consumer);
+
+                return (consumer);
+            }
+        }
+    }
+}
